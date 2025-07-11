218255aa3b04634c4335248cce6ef805ecb5afa8
Neeraj kumar <abhikrgkp1993@gmail.com>
2025-07-12 00:36:23 +0530

    Add user signup and login functionality with Passport.js

--- app.js
+++ app.js
@@ -7,15 +7,18 @@
   const ExpressError = require("./utils/ExpressError.js");
   const session = require("express-session");
   const flash = require("connect-flash");
-
+  const passport = require("passport");
+  const LocalStrategy = require("passport-local");
+  const User = require("./models/user.js"); 
   const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
   const path = require("path");
   const methodOverride = require("method-override");
 
 
-  const listings = require("./routes/listing.js");
-  const reviews = require("./routes/review.js");
+  const listingRouter = require("./routes/listing.js");
+  const reviewRouter = require("./routes/review.js");
+   const userRouter = require("./routes/user.js");
   main()
     .then(() => {
       console.log("connected to DB");
@@ -54,12 +57,34 @@
  app.use(session(sessionOptions));
  app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+// use static authenticate method of model in  localStrategy 
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize a nd deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error"); 
 
     next();
 });
+ 
+// app.get("/demouser",async(req, res)=>{
+//   let fakeUser = new User ({
+//     email : "student@gmail.com",
+//     username : "Ravi-kumar",
+//   });
+//   let registeredUser = await User.register(fakeUser ,"helloworld");
+//   res.send(registeredUser);
+// });
+ 
+
+
+
   //Middleware 
   const validateListing = (req,res,next)=>{
         let {error} = listingSchema.validate(req.body);
@@ -85,8 +110,9 @@ app.use((req,res,next)=>{
     
   }
 
-  app.use("/listings",listings);
-  app.use("/listings/:id/reviews",reviews);
+  app.use("/listings",listingRouter);
+  app.use("/listings/:id/reviews",reviewRouter);
+  app.use("/",userRouter);
 
 
   // <-------THis error check after project completion ------>
